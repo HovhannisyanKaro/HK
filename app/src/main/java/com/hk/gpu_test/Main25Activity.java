7c13f67594d6b05a0a3f4b3f73b5a1531d20ccfe
HovhannisyanKaro <Hovhannisyankaro1992@gmail.com>
2018-01-17 21:41:51 +0400

    test committ

--- app/src/main/java/com/hk/gpu_test/Main25Activity.java
+++ app/src/main/java/com/hk/gpu_test/Main25Activity.java
@@ -19,6 +19,8 @@ public class Main25Activity extends AppCompatActivity {
 
     GPUImageFilter mFilter;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
